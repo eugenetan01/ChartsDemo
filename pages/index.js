0fa2965a2d84d0a78c81f00f6e2281130d19cc89
Eugene Tan <>
2022-04-06 14:02:27 +0800

    added mongodb logo

--- pages/index.js
+++ pages/index.js
@@ -56,13 +56,18 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.mongodb.com"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{" "}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image
+              src="/mongodb.png"
+              alt="MongoDB logo"
+              width={72}
+              height={19}
+            />
           </span>
         </a>
       </footer>
